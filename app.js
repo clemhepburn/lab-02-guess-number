0c762ee7c6ab11e48e3d2af179513c85b110a071
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-03-25 08:16:41 -0700

    try to test and also write if statements in event listener

--- app.js
+++ app.js
@@ -9,3 +9,19 @@ let correctNumber = Math.ceil(Math.random() * 10);
 button.addEventListener('click', () => {
     guesses--;
     console.log(compareNumbers(guessInput.value, correctNumber));
+
+    if (compareNumbers(guessInput.value, correctNumber) === 'less than') {
+        winLose.textContent = 'That guess is too high! You have ' + guesses + ' guesses left!';
+        
+    }
+    else if (compareNumbers(guessInput.value, correctNumber) === 'greater than') {
+        winLose.textContent = 'That guess is too low! You have ' + guesses + ' guesses left!'; 
+    } 
+    else if (compareNumbers(guessInput.value, correctNumber) === 'equal') {
+        winLose.textContent = 'You got it!';
+    }
+    else if (guesses <= 0) {
+        button.removeEventListener;
+        winLose.textContent = 'You ran out of guesses.';
+    }
+});
