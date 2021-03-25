aa183fe461d8a55796b7250dbb19b4320c733cb6
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-03-25 08:05:53 -0700

    build initialize state of guesses and random number

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ const button = document.getElementById('submit-button');
 const winLose = document.getElementById('win-lose');
 const guessInput = document.getElementById('guess-input');
 
+let guesses = 5;
+let correctNumber = Math.ceil(Math.random() * 10);
